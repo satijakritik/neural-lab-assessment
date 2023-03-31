0f9b7138615a9834576cff04c9a25976b0dfa628
Kritik Satija <kritik.satija@neurallab.io>
2023-03-31 17:19:40 +0800

    Added docx for q1 and logging for q2 in fast_api.py file

--- fast_api.py
+++ fast_api.py
@@ -2,6 +2,15 @@ import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='logging.csv'
+)
+logger = logging.getLogger(__name__)
 
 class Item(BaseModel):
     file: str
@@ -10,6 +19,8 @@ app = FastAPI()
 
 @app.post("/process")
 def process_excel_file(item: Item):
+    logger.info('Process API call received')
+    
     data = json.loads(item.file)
     log = data['log_type']
     classification = data['classification_type']
@@ -17,6 +28,7 @@ def process_excel_file(item: Item):
     result = {"log_type": {}, "classification_type": {}}
     
     log_vals = list(dict(log).values())
+    
     unique_log_vals = list(set(log_vals))
     for log_type in unique_log_vals:
         result["log_type"][log_type] = log_vals.count(log_type)
@@ -25,5 +37,7 @@ def process_excel_file(item: Item):
     unique_classification_vals = list(set(classification_vals))
     for classification_type in unique_classification_vals:
         result["classification_type"][classification_type] = classification_vals.count(classification_type)
+        
+    logger.info(f'Processing Completed! result = {result}')
         
     return result
